f765a22bea7dbe577116c9cb56578821f58b5d57
Chris Hosegood <chris.hosegood@gmail.com>
2012-04-17 14:28:13 +0100

    Added uglification and concatination

--- build/build.js
+++ build/build.js
@@ -3,5 +3,6 @@
     mainConfigFile: '../js/main.js',
     name: '../js/main',
     out: '../js/scripts.min.js',
-    optimize: 'none',
+    optimize: 'uglify',
+    preserveLicenseComments: false
 })
